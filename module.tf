02cac8261d0de50826d2c8dfc71f1e0a2f0c55e1
Ayomide Olubanwo <ayomideolubanwo@Ayomides-MacBook-Air.local>
2023-10-31 19:00:35 -0400

    first commit

--- module.tf
+++
@@ -1,3 +0,0 @@
-module "kubernetes" {
-  source = "git::https://github.com/Ayomi234/eks-roles.git?ref=v1.0.1"
-}
--- oidc-test/provider.tf
+++ oidc-test/provider.tf
@@ -1,12 +1,12 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~>3.0"
+      version = "~> 3.0"
     }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
